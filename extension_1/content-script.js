8e9d7a93178aa7a506da2f05f0e46fa72442c8d6
Roma Golev <92201592+RomaG01@users.noreply.github.com>
2023-03-11 11:23:28 +0500

    Add files via upload

---
+++ extension_1/content-script.js
@@ -0,0 +1,20 @@
+function changeBlock(colorValue) {
+    // !!document
+    // .getElementsByClassName.style.backgroundColor(
+    //     "ContributionCalendar-day"
+    //     ) &&
+    //     document
+    // .getElementsByClassName.style.backgroundColor(
+    //     "ContributionCalendar-day"
+    //     )[0].remove();
+        
+    // const elem = document.getElementsByClassName.style.backgroundColor("ContributionCalendar-day")
+    // elem.map ('click', () => {
+    //     console.log(123);
+    //     elem.style.backgroundColor = '#50b725';
+    // })
+
+    document.ContributionCalendar-day.style.background == document.getElementById(colorValue).dataset.color
+}
+
+changeBlock()
